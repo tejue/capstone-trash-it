bf67140f0a71ea450be62b23fec9c103120be2bb
teresa <teresa.juengel@posteo.de>
2024-01-08 17:02:24 +0100

    add GameRepo

--- backend/src/main/java/tejue/backend/repo/GameRepo.java
+++ backend/src/main/java/tejue/backend/repo/GameRepo.java
@@ -1,4 +1,9 @@
 package tejue.backend.repo;
 
-public class GameRepo {
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import tejue.backend.model.Result;
+
+@Repository
+public interface GameRepo extends MongoRepository<Result, String> {
 }
