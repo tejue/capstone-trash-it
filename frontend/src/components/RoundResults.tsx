922ee610280099cd2f88dc607f9c06eba999fe1c
teresa <teresa.juengel@posteo.de>
2024-01-11 20:10:21 +0100

    add tiny styling

--- frontend/src/components/RoundResults.tsx
+++ frontend/src/components/RoundResults.tsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import axios from "axios";
 import {RoundType} from "../types/RoundType.ts";
+import styled from "styled-components";
 
 export default function RoundResults() {
 
@@ -20,11 +21,16 @@ export default function RoundResults() {
             <button onClick={getRoundsResults}>Click</button>
             {rounds
                 .map((round, index) => (
-                    <section key={index}>
+                    <StyledSection key={index}>
                         <p>{`Round: ${round.roundNumber}`}</p>
                         <p>{`You trashed ${round.trashedTotal} / ${round.garbageTotal}`}</p>
-                    </section>
+                    </StyledSection>
                 ))}
         </>
     );
 }
+
+const StyledSection = styled.section`
+  display: flex;
+  gap: 20px;
+`
