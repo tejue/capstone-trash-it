41d36484a076b99a274a9adc23d1b6150dc14327
teresa <teresa.juengel@posteo.de>
2024-01-11 20:33:31 +0100

    implement routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css'
 import RoundResults from "./components/RoundResults.tsx";
+import {Route, Routes} from "react-router-dom";
 
 export default function App() {
 
     return (
-        <RoundResults/>
+        <>
+            <Routes>
+                <Route path={"/rounds"} element={<RoundResults/>}></Route>
+            </Routes>
+        </>
     )
 }
