68880afbed16ac2374e7e3d136a29950217fd072
teresa <teresa.juengel@posteo.de>
2024-01-11 19:54:31 +0100

    add functionality to round results

--- frontend/src/components/RoundResults.tsx
+++ frontend/src/components/RoundResults.tsx
@@ -1,17 +1,30 @@
 import {useState} from "react";
 import axios from "axios";
+import {RoundType} from "../types/RoundType.ts";
 
 export default function RoundResults() {
 
-    const [results, setResults] = useState([])
+    const [rounds, setRounds] = useState<RoundType[]>([])
 
-    function getResultsOfPlayer(playerId: number) {
-        axios.get("/api/game/1/results")
-            .then(response => setResults(response.data))
-        console.log(results)
+    const playerId: string = "1"
+
+    function getRoundsResults() {
+        axios.get(`/api/game/${playerId}/rounds`)
+            .then(response => {
+                setRounds(response.data);
+            })
     }
 
     return (
-        <button onClick={getResultsOfPlayer}>Click</button>
-    )
+        <>
+            <button onClick={getRoundsResults}>Click</button>
+            {rounds
+                .map((round, index) => (
+                    <section key={index}>
+                        <p>{`Round: ${round.roundNumber}`}</p>
+                        <p>{`You trashed ${round.trashedTotal} / ${round.garbageTotal}`}</p>
+                    </section>
+                ))}
+        </>
+    );
 }
