3538bed64d3defa76dcb8d2b80cff4deccb876d3
teresa <teresa.juengel@posteo.de>
2024-01-12 21:05:04 +0100

    add unit tests for TrashService and TrashCanService

---
+++ backend/src/test/java/tejue/backend/service/TrashServiceTest.java
@@ -0,0 +1,12 @@
+package tejue.backend.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrashServiceTest {
+
+    @Test
+    void getRandomMax() {
+    }
+}
