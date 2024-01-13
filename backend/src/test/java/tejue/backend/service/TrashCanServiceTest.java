889c28b2b18116f2430aee3891a1e83371fe0261
teresa <teresa.juengel@posteo.de>
2024-01-13 17:37:38 +0100

    add unit tests for TrashCanService

--- backend/src/test/java/tejue/backend/service/TrashCanServiceTest.java
+++ backend/src/test/java/tejue/backend/service/TrashCanServiceTest.java
@@ -1,12 +1,48 @@
 package tejue.backend.service;
 
 import org.junit.jupiter.api.Test;
+import tejue.backend.model.TrashCan;
+import tejue.backend.model.TrashType;
+import tejue.backend.repo.TrashCanRepo;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TrashCanServiceTest {
 
+    private final TrashCanRepo trashCanRepo = mock(TrashCanRepo.class);
+    private final TrashCanService trashCanService = new TrashCanService(trashCanRepo);
+
     @Test
-    void getAllTrashCans() {
+    void getAllTrashCans_whenAllTrashCansAreCalled_thenReturnAllTrashCans() {
+        //GIVEN
+        List<TrashCan> expectedTrashCans = List.of(new TrashCan("1", "Paper Can", "blue", "", TrashType.PAPER, List.of("1", "7", "10")));
+        trashCanRepo.saveAll(expectedTrashCans);
+
+        //WHEN & THEN
+        when(trashCanRepo.findAll()).thenReturn(expectedTrashCans);
+
+        List<TrashCan> actual = trashCanService.getAllTrashCans();
+        verify(trashCanRepo).findAll();
+        assertEquals(expectedTrashCans, actual);
+    }
+
+    @Test
+    void getAllTrashCans_whenNoTrashCanIsSaved_thenReturnEmptyList() {
+        //GIVEN
+        List<TrashCan> expectedTrashCans = List.of();
+        trashCanRepo.saveAll(expectedTrashCans);
+
+        //WHEN & THEN
+        when(trashCanRepo.findAll()).thenReturn(Collections.emptyList());
+
+        List<TrashCan> actual = trashCanService.getAllTrashCans();
+        verify(trashCanRepo).findAll();
+        assertEquals(expectedTrashCans, actual);
     }
 }
