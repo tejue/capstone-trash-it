f93d4a53bcbe696fe2b3eb93d655a6f88893946d
teresa <teresa.juengel@posteo.de>
2024-01-15 20:41:03 +0100

    adjust trash and trashCan components to dnd

--- frontend/src/components/Trash.tsx
+++ frontend/src/components/Trash.tsx
@@ -1,28 +1,19 @@
-import {useEffect, useState} from "react";
-import axios from "axios";
 import {TrashType} from "../types/TrashType.ts";
+import DraggableTrash from "./DraggableTrash.tsx";
 import styled from "styled-components";
 
-export default function Trash() {
+type TrashProps = {
+    trashes: TrashType [];
+}
 
-    const [trashes, setTrashes] = useState<TrashType[]>([])
-
-    useEffect(() => {
-            getTrashes()
-        }, []
-    )
-
-    function getTrashes() {
-        axios.get("api/trash")
-            .then(response => setTrashes((response.data)))
-    }
+export default function Trash(props: TrashProps) {
 
     return (
         <StyledSection>
-            {trashes.map((trash, index) => (
-                <StyledTrash key={trash.id} index={index + 1}>
-                    {`${trash.name}`}</StyledTrash>
-            ))}
+            {props.trashes.map((trash, index) => (
+                <DraggableTrash key={trash.id} trash={trash} index={index + 1}/>
+            ))
+            }
         </StyledSection>
     )
 }
@@ -40,15 +31,4 @@ const StyledSection = styled.section`
     "area5 area5 area6 area6 area8 area8 area9 area9"
     "area5 area5 area6 area6 area8 area8 area9 area9"
     "area11 area11 area11 area10 area10 area12 area12 area12";
-`;
-
-const StyledTrash = styled.div<{ index: number }>`
-  grid-area: ${(props) => `area${props.index}`};
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: 1px solid white;
-  padding: 8px;
-  height: 100%;
-  box-sizing: border-box;
 `;
