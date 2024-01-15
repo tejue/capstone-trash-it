60ac60f3df05a45885ff7415bf4b4eb1c725cb18
teresa <teresa.juengel@posteo.de>
2024-01-15 09:53:38 +0100

    add test for trashService

--- backend/src/test/java/tejue/backend/service/TrashServiceTest.java
+++ backend/src/test/java/tejue/backend/service/TrashServiceTest.java
@@ -1,12 +1,40 @@
 package tejue.backend.service;
 
 import org.junit.jupiter.api.Test;
+import tejue.backend.model.Trash;
+import tejue.backend.model.TrashType;
+import tejue.backend.repo.TrashRepo;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class TrashServiceTest {
 
+    private final TrashRepo trashRepo = mock(TrashRepo.class);
+    private final TrashService trashService = new TrashService(trashRepo);
+
     @Test
-    void getRandomMax() {
+    void getRandomTrashMax_whenCalled_thenReturnListOfTrash() {
+        //GIVEN
+        List<Trash> expectedTrash = Arrays.asList(
+                new Trash("1", "Newspaper", "", TrashType.PAPER),
+                new Trash("2", "Plastic Bottle", "", TrashType.PLASTIC),
+                new Trash("3", "Yoghurt Cup", "", TrashType.PLASTIC)
+        );
+        trashRepo.saveAll(expectedTrash);
+
+        when(trashRepo.findAll()).thenReturn(expectedTrash);
+
+        //WHEN
+        List<Trash> actual = trashService.getRandomTrashMax();
+
+        //THEN
+        verify(trashRepo).findAll();
+        assertFalse(actual.isEmpty());
+        assertTrue(actual.size() <= 10);
+        assertEquals(expectedTrash, actual);
     }
 }
