69ba629ad0d981f383a29e001d644ab7fc9cbfd2
teresa <teresa.juengel@posteo.de>
2024-01-15 20:40:16 +0100

    implement draggable and droppable component

---
+++ frontend/src/components/DraggableTrash.tsx
@@ -0,0 +1,40 @@
+import {TrashType} from "../types/TrashType.ts";
+import styled from "styled-components";
+import {CSS} from "@dnd-kit/utilities";
+import {useDraggable} from "@dnd-kit/core";
+
+type DraggableTrashProps = {
+    trash: TrashType;
+    index: number;
+}
+export default function DraggableTrash(props: DraggableTrashProps) {
+
+    const {attributes, listeners, setNodeRef, transform} = useDraggable({id: props.trash.id});
+
+    return (
+        <StyledDraggableTrash
+            id={props.trash.id}
+            ref={setNodeRef}
+            index={props.index}
+            transform={transform}
+            {...attributes}
+            {...listeners}>
+            {`${props.trash.name}`}
+        </StyledDraggableTrash>
+    )
+}
+
+const StyledDraggableTrash = styled.div<{
+    index: number;
+    transform: any;
+}>`
+  grid-area: ${(props) => `area${props.index}`};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid white;
+  padding: 8px;
+  height: 100%;
+  box-sizing: border-box;
+  transform: ${(props) => CSS.Transform.toString(props.transform)};
+`;
---
+++ frontend/src/components/DroppableTrashCan.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import {useDroppable} from "@dnd-kit/core";
+import {TrashCanType} from "../types/TrashCanType.ts";
+
+type DroppableTrashCanProps = {
+    trashCan: TrashCanType;
+}
+
+export default function DroppableTrashCan(props: DroppableTrashCanProps) {
+
+    const {isOver, setNodeRef} = useDroppable({
+        id: props.trashCan.id,
+    });
+
+    const style = {
+        opacity: isOver ? 1 : 0.5,
+    }
+
+    return (
+        <StyledDroppableTrashCan
+            id={props.trashCan.id}
+            ref={setNodeRef}
+            style={style}>
+            {`${props.trashCan.name}`}
+        </StyledDroppableTrashCan>
+    )
+}
+
+const StyledDroppableTrashCan = styled.div`
+  border: solid white 1px;
+  text-align: center;
+`
