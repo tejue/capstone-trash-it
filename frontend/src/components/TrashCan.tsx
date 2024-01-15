1876b28be9af7781fc132d6c03d7de95fae5fe2c
teresa <teresa.juengel@posteo.de>
2024-01-15 12:45:13 +0100

    add basic styling trashCans

--- frontend/src/components/TrashCan.tsx
+++ frontend/src/components/TrashCan.tsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 import {TrashCanType} from "../types/TrashCanType.ts";
+import styled from "styled-components";
 
 export default function Game() {
 
@@ -17,13 +18,26 @@ export default function Game() {
             .then(response => setTrashCans((response.data)))
     }
 
-    console.log(trashCans);
     return (
-        <>
+
+            <StyledSection>
             {trashCans.map((trashCan) => (
-                <section key={trashCan.id}>
-                    <p>{`${trashCan.name}`}</p>
-                </section>))}
-        </>
+                <StyledTrashCan key={trashCan.id}>
+                    {`${trashCan.name}`}
+                </StyledTrashCan>))}
+            </StyledSection>
+
     )
 }
+
+const StyledSection = styled.section`
+  display: flex;
+  gap: 10px;
+  margin: 20px;
+  justify-content: center;
+`
+
+const StyledTrashCan = styled.div`
+  border: solid white 1px;
+  text-align: center;
+`
