2b9625904535d90ff8989484743304d06a7460c2
teresa <teresa.juengel@posteo.de>
2024-01-19 15:17:53 +0100

    add models for result

---
+++ backend/src/main/java/tejue/backend/model/SetOfPoints.java
@@ -0,0 +1,15 @@
+package tejue.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SetOfPoints {
+    private String trashCanId;
+    private int playerPoints;
+    private int dataPoints;
+}
