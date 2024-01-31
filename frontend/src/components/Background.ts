549755b57ad99c4db3237a4e7aabc0f9a8330932
teresa <teresa.juengel@posteo.de>
2024-01-31 14:12:59 +0100

    add background component

---
+++ frontend/src/components/Background.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+type BackgroundProps = {
+    $backgroundColor?: string;
+}
+
+export const Background = styled.div<BackgroundProps>`
+   content: "";
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${({$backgroundColor}) => $backgroundColor === "none" ? "transparent" : "rgba(76, 167, 166, 0.6)"};
+  backdrop-filter: blur(0.1px);
+  z-index: -1;
+`;
