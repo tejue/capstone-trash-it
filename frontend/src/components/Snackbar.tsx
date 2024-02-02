587e275c331c72056791f6adc306f9f1a6eabed7
teresa <teresa.juengel@posteo.de>
2024-02-02 15:55:56 +0100

    change snackbar for multiple use

--- frontend/src/components/Snackbar.tsx
+++ frontend/src/components/Snackbar.tsx
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 type SnackbarProps = {
     message: string;
-    onClick: () => void;
+    onClickOk: () => void;
+    onClickCancel?: () => void;
 }
 
 export default function Snackbar(props: Readonly<SnackbarProps>) {
@@ -10,7 +11,8 @@ export default function Snackbar(props: Readonly<SnackbarProps>) {
     return (
         <StyledSnack>
             <SnackMessage>{props.message}</SnackMessage>
-            <SnackButton onClick={props.onClick}>Alright</SnackButton>
+            <SnackButton onClick={props.onClickOk}>Alright</SnackButton>
+            {props.onClickCancel && <SnackButton onClick={props.onClickCancel}>Cancel</SnackButton>}
         </StyledSnack>
     );
 }
@@ -18,18 +20,20 @@ export default function Snackbar(props: Readonly<SnackbarProps>) {
 const StyledSnack = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 15px;
   position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   width: 100%;
+  padding: 30px;
   background-color: rgba(54, 54, 54, 0.87);
   color: var(--text-color-light);
   text-align: center;
   z-index: 5;
 `
 const SnackMessage = styled.span`
-  margin: 40px;
+  margin-bottom: 10px;
 `
 
 const SnackButton = styled.button`
@@ -37,6 +41,6 @@ const SnackButton = styled.button`
   border: var(--secondary-color) solid thin;
   border-radius: 5px;
   color: var(--text-color-light);
-  margin: 0 40px 40px 40px;
+  //margin: 0 40px 40px 40px;
   padding: 5px;
 `
